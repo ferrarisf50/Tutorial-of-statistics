4a51258401610ceaf64eb29df79b288b12e22982
ferrarisf50 <ferrarisf50@hotmail.com>
2018-10-09 17:32:20 -0400

    update: update the script.

--- bootstrap.r
+++ bootstrap.r
@@ -1,33 +1,84 @@
-set.seed(123456)
-data10000=rnorm(10000,0,1)
-data1000=rnorm(1000,0,1)
-data100=rnorm(100,0,1)
-data10=rnorm(10,0,1)
-data5=rnorm(5,0,1)
+seeding=1234567
+set.seed(seeding)
+
+data10000 <- rnorm(10000,0,1)
+data1000 <- rnorm(1000,0,1)
+data100 <- rnorm(100,0,1)
+data10 <- rnorm(10,0,1)
+data5 <- rnorm(5,0,1)
+
+datalist <- list(data10000,data1000,data100,data10,data5)
 
 library(MASS)
 library(boot)
 #sample mean
-mean(data10000, na.rm=TRUE)
-mean(data1000, na.rm=TRUE)
-mean(data100, na.rm=TRUE)
-mean(data10, na.rm=TRUE)
-mean(data5, na.rm=TRUE)
 
-#standard deviation of the sample
+sampling_mean <- numeric(5)
+for (i in 1:5) {
+  sampling_mean[i] <- mean(datalist[[i]], na.rm=TRUE)
+  }
 
-sqrt(sum((data5 - mean(data5))^2) / (length(data5) - 1))
-sd(data5)
+sampling_mean
 
+#standard deviation of the population mean
+
+mean_se <- numeric(5)
+for (i in 1:5) {
+  mean_se[i] <- 1/sqrt(length(datalist[[i]]))
+}
+
+mean_se
 
 #standard error of the mean
-sd(data10000)/sqrt(length(data10000))
-sd(data1000)/sqrt(length(data1000))
-sd(data100)/sqrt(length(data100))
-sd(data10)/sqrt(length(data10))
-sd(data5)/sqrt(length(data5))
 
+sampling_se <- numeric(5)
+for (i in 1:5) {
+  
+  sampling_se[i] <- sd(datalist[[i]])/sqrt(length(datalist[[i]]))
+}
+
+sampling_se
+
+# resampling mean
+
+
+
+resampling_mean <- numeric(5)
+for (i in 1:5) {
+  set.seed(seeding)
+  dim <- length(datalist[[i]])*10
+  resampling_mean[i] <- mean(sample(datalist[[i]],dim,replace=T), na.rm=TRUE)
+}
+
+resampling_mean
+
+resampling_se <- numeric(5)
+for (i in 1:5) {
+  set.seed(seeding)
+  dim <- length(datalist[[i]])*10
+  data_re=sample(datalist[[i]],dim,replace=T)
+  resampling_se[i] <- sd(data_re)/sqrt(length(data_re))
+}
+
+resampling_se
+
+#bootstrap
+
+boot10000 <-numeric(1000)
+boot1000 <-numeric(1000)
+boot100 <-numeric(1000)
+boot10 <-numeric(1000)
+boot5 <-numeric(1000)
+
+for (i in 1:1000) boot10000[i] <- mean(sample(data10000,replace=T))
+for (i in 1:1000) boot1000[i] <- mean(sample(data1000,replace=T))
+for (i in 1:1000) boot100[i] <- mean(sample(data100,replace=T))
+for (i in 1:1000) boot10[i] <- mean(sample(data10,replace=T))
+for (i in 1:1000) boot5[i] <- mean(sample(data5,replace=T))
+
+mean(boot10000)
 
 results <- boot(data=data10, statistic=rsq, 
                 R=1000)
+
 
